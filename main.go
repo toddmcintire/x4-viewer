5647d8199fc5054137526afbd982f58f3efcacc5
Todd McIntire <mail@toddmcintire.com>
2026-01-06 09:25:01 -0600

    removed prints

--- main.go
+++ main.go
@@ -30,14 +30,6 @@ func main() {
 				expanded := pbm.ExpandBitmap(buf)
 				img := rl.NewImage(expanded, 480, 800, 1, rl.UncompressedGrayscale)
 				texture = rl.LoadTextureFromImage(img)
-				
-				if bytesRead > 0 {
-					fmt.Printf("%v ", bytesRead)
-					for _, v := range buf{
-						fmt.Printf("%v ", v)
-					}
-					fmt.Println()
-				}
 			}
 
 			rl.UnloadDroppedFiles()
@@ -47,32 +39,9 @@ func main() {
 		rl.BeginDrawing()
 		rl.ClearBackground(rl.RayWhite)
 		if len(filePaths) == 0 {
-			rl.DrawText("Drop file", 100, 40, 20, rl.DarkGray)
+			rl.DrawText("Drop file", 200, 400, 20, rl.DarkGray)
 		} else {
 			rl.DrawTexture(texture, 0, 0, rl.RayWhite)
-			//rl.DrawText("Dropped files:", 100, 40, 20, rl.DarkGray)
-
-			//for i:=int32(0); i < int32(len(filePaths)); i++ {
-				// if i%2 == 0 {
-				// 	rl.DrawRectangle(0, 85+40*i, 480, 40, rl.Fade(rl.LightGray, 0.5))
-				// } else {
-				// 	rl.DrawRectangle(0, 85+40*i, 480, 40, rl.Fade(rl.LightGray, 0.3))
-				// }
-				
-				// rl.DrawText(filePaths[i], 120, 100 + 40 * i, 10, rl.Gray)
-				//fmt.Println(filePaths[i])
-				
-				//if strings.Contains(filePaths[i], "xtg") {
-					//x4.GetXTGData(filePaths[i])
-					//expanded := pbm.ExpandBitmap(pictureData)
-					//fmt.Println(pictureData)
-					// img := rl.NewImage(expanded, 480, 800, 1, rl.UncompressedGrayscale)
-					// texture := rl.LoadTextureFromImage(img)
-					// rl.DrawTexture(texture,0,0,rl.RayWhite)
-				//}
-
-			//}
-			//rl.DrawText("drop new files...", 100, 110 + 40 * int32(filepathCounter), 20, rl.DarkGray)
 		}
 		rl.EndDrawing()
 	}
